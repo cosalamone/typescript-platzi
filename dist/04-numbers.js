1195cf4bc169df268a83a3d995ebf2ae5868a5a9
cosalamone <cosalamone@gmail.com>
2022-07-24 11:15:48 -0300

    04-numbers

---
+++ dist/04-numbers.js
@@ -0,0 +1,16 @@
+"use strict";
+(() => {
+    let productPrice = 123;
+    productPrice = 12;
+    console.log(productPrice, "productPrice");
+    let customerAge = 28;
+    customerAge = customerAge + 1;
+    let productInStock;
+    console.log(productInStock);
+    "productInStock";
+    if (productInStock > 10) {
+        console.log("is greater");
+    }
+    let discount = parseInt("123");
+    console.log(discount, "discount");
+})();
---
+++ src/04-numbers.ts
@@ -0,0 +1,19 @@
+(() => {
+  let productPrice = 123;
+  productPrice = 12;
+  console.log(productPrice, "productPrice")
+
+  let customerAge: number = 28;
+  customerAge = customerAge + 1
+
+  let productInStock: number;
+  console.log(productInStock; "productInStock")
+  if (productInStock>10){
+    console.log("is greater");
+  }
+
+  let discount = parseInt("123");
+  console.log(discount, "discount");
+
+
+})();
