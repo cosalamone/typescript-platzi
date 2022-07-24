6df83a702ba53ac95341a27367d068134c179bef
cosalamone <cosalamone@gmail.com>
2022-07-24 11:14:09 -0300

    03-typing

---
+++ dist/03-typing.js
@@ -0,0 +1,10 @@
+"use strict";
+(() => {
+    let myProductName = "product 1";
+    let myProductPrice = 123;
+    myProductName = "asda";
+    myProductName.toLocaleLowerCase();
+    myProductPrice.toFixed();
+    const myProductStock = 1000;
+    const myProductName2 = "product 1";
+})();
---
+++ src/03-typing.ts
@@ -0,0 +1,15 @@
+(()=> {
+
+  let myProductName = "product 1";
+  let myProductPrice = 123;
+
+  myProductName = "asda";
+  myProductName.toLocaleLowerCase();
+
+  myProductPrice.toFixed();
+
+  const myProductStock = 1000;
+  const myProductName2 = "product 1";
+
+})();
+
