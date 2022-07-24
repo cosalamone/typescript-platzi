db17b85ca3a8ec2251298f2b1c0882e690a00375
cosalamone <cosalamone@gmail.com>
2022-07-24 11:17:37 -0300

    06-strings

---
+++ src/06-strings.ts
@@ -0,0 +1,14 @@
+(() => {
+  let producTitile = "My suculent";
+  producTitile = "Haworthia";
+
+  console.log(producTitile, "producTitile");
+
+  const productDescription = "La descripcion del prod"
+
+  const summary = `
+  title: ${producTitile}
+  desciption: ${productDescription}`
+
+  console.log(summary)
+})();
