401365b8b8bc7de34b1819a0dfe37220faac2d0a
cosalamone <cosalamone@gmail.com>
2022-07-24 11:19:23 -0300

    09-union-types

---
+++ src/09-union-types.ts
@@ -0,0 +1,17 @@
+(() => {
+  let userID: string | number;
+  userID = 1212;
+  userID = "asd";
+
+  function greeting(myText: string | number) {
+    if (typeof myText === "string") {
+      console.log(`string ${myText.toLocaleUpperCase()}`)
+    }
+    else {
+      console.log(`number ${myText.toFixed(2)}`)
+    }
+  }
+  greeting("hola")
+
+  greeting(1234.745632142)
+})();
